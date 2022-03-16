35bf3229627f6d46b999b7cfbe365f3687e90998
Rian Perassoli <rian.perassoli@gmail.com>
2022-03-16 10:43:33 -0300

    condigurado debugger Node

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => response.json({ message: "Hello world" }));
 
 app.listen(3333, () => console.log("Server is running!"));
