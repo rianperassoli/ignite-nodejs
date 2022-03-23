a5423aaaa5f6b49e62ce66c368b7dc202efc4338
Rian Perassoli <rian.perassoli@gmail.com>
2022-03-23 09:30:14 -0300

    Criada a tabela de usuário e efetuado método de criação de uusário

---
+++ rentalx/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,31 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUsersDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/user";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUsersDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
