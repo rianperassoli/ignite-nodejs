fff443283e21c58ceab3e90f77c72f0b48897fcc
Rian Perassoli <rian.perassoli@gmail.com>
2022-04-06 10:36:54 -0300

    Criadas regras de atualização do refresh token

--- rentalx/src/shared/infra/http/middlewares/ensureAuthenticate.ts
+++ rentalx/src/shared/infra/http/middlewares/ensureAuthenticate.ts
@@ -3,6 +3,7 @@ import { verify } from "jsonwebtoken";
 
 import auth from "@config/auth";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -16,6 +17,8 @@ export async function ensureAuthenticate(
 ) {
   const authHeader = request.headers.authorization;
 
+  const usersTokensRepository = new UsersTokensRepository();
+
   if (!authHeader) {
     throw new AppError("Token missing", 401);
   }
@@ -23,11 +26,15 @@ export async function ensureAuthenticate(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
+    const { sub: user_id } = verify(
+      token,
+      auth.secret_refresh_token
+    ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-
-    const user = await usersRepository.findById(user_id);
+    const user = await usersTokensRepository.findByUserAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError("User does not exist", 401);
