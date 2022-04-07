404627496cd7bc803ecb4844e0a64b30ab348917
Rian Perassoli <rian.perassoli@gmail.com>
2022-04-07 10:33:58 -0300

    Criada rota para fazer envio de emaol de esquecimento de senha

--- rentalx/src/modules/accounts/UseCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ rentalx/src/modules/accounts/UseCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -4,6 +4,7 @@ import { v4 as uuidV4 } from "uuid";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
 
 @injectable()
@@ -14,7 +15,9 @@ class SendForgotPasswordMailUseCase {
     @inject("UsersTokensRepository")
     private usersTokensRepository: IUsersTokensRepository,
     @inject("DateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("MailProvider")
+    private mailProvider: IMailProvider
   ) {}
 
   async execute(email: string): Promise<void> {
@@ -24,13 +27,20 @@ class SendForgotPasswordMailUseCase {
       throw new AppError("User does not exists!");
     }
 
+    const refresh_token = uuidV4();
     const expires_date = this.dateProvider.addHours(3);
 
     await this.usersTokensRepository.create({
-      refresh_token: uuidV4(),
+      refresh_token,
       user_id: user.id,
       expires_date,
     });
+
+    await this.mailProvider.sendEmail(
+      email,
+      "Recuperação de senha",
+      `O link para o reset é ${refresh_token}`
+    );
   }
 }
 
