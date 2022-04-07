64d9021035d29867f75deca1663bf668a206e14e
Rian Perassoli <rian.perassoli@gmail.com>
2022-04-07 11:19:38 -0300

    Criado template para email de esqueci a senha

--- rentalx/src/modules/accounts/UseCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ rentalx/src/modules/accounts/UseCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from "uuid";
 
@@ -23,6 +24,15 @@ class SendForgotPasswordMailUseCase {
   async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
+    const templatePath = resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "emails",
+      "forgotPassword.hbs"
+    );
+
     if (!user) {
       throw new AppError("User does not exists!");
     }
@@ -36,10 +46,16 @@ class SendForgotPasswordMailUseCase {
       expires_date,
     });
 
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${refresh_token}`,
+    };
+
     await this.mailProvider.sendEmail(
       email,
       "Recuperação de senha",
-      `O link para o reset é ${refresh_token}`
+      variables,
+      templatePath
     );
   }
 }
--- rentalx/src/shared/container/providers/index.ts
+++ rentalx/src/shared/container/providers/index.ts
@@ -7,13 +7,13 @@ import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMai
 
 container.registerSingleton<IDateProvider>("DateProvider", DayJSDateProvider);
 
-container.registerSingleton<IMailProvider>(
-  "MailProvider",
-  EtherealMailProvider
-);
+// container.registerSingleton<IMailProvider>(
+//   "MailProvider",
+//   EtherealMailProvider
+// );
 
 // Caso nao funcionar o envio de email, utilizar a criação desta forma
-// container.registerInstance<IMailProvider>(
-//   "MailProvider",
-//   new EtherealMailProvider()
-// );
+container.registerInstance<IMailProvider>(
+  "MailProvider",
+  new EtherealMailProvider()
+);
