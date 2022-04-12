7276e07365265a8db9f40d721890c9dbcb8a3e42
Rian Perassoli <rian.perassoli@gmail.com>
2022-04-12 15:09:09 -0300

    Criado envio de avatars para o s3

--- rentalx/src/shared/container/providers/index.ts
+++ rentalx/src/shared/container/providers/index.ts
@@ -4,16 +4,23 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayJSDateProvider } from "./DateProvider/implementations/DayJSDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
+import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayJSDateProvider);
 
-// container.registerSingleton<IMailProvider>(
-//   "MailProvider",
-//   EtherealMailProvider
-// );
-
-// Caso nao funcionar o envio de email, utilizar a criação desta forma
 container.registerInstance<IMailProvider>(
   "MailProvider",
   new EtherealMailProvider()
+);
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  "StorageProvider",
+  diskStorage[process.env.disk]
 );
