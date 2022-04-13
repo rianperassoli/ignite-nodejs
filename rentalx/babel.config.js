83db1e06dddcf3c3512cf7d2354ed78190682dcd
Rian Perassoli <rian.perassoli@gmail.com>
2022-04-13 14:43:23 -0300

    ajuste babel

--- rentalx/babel.config.js
+++ rentalx/babel.config.js
@@ -17,7 +17,8 @@ module.exports = {
       },
     ],
     "babel-plugin-transform-typescript-metadata",
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
   ],
 };
